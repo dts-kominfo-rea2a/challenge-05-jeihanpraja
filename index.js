a26295048dd614ff29f4da7cd171fdda7f70734c
Jeihan Praja Yudanto <jeihan.yudanto@gmail.com>
2022-06-05 10:51:13 +0700

    challenge-04-versi1

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = function(names, sortType) {
+  sortType(names); // untuk fungsi callback
+  
+  let arrNamesSorted = []; //wadah array sorted berdasarkan sortType
+  for (i = 0; i < names.length; i++) {
+      //tambahkan nomor urut dan masukkan ke wadah arrNamesSorted
+      arrNamesSorted.push( (i+1) + ". " + names[i] );
+  }
+  return arrNamesSorted;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = function(names) {
+  return names.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = function(names) {
+  return names.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
